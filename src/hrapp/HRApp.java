4c0dfd91aaf155cbce651577caecbb24dfa808bc
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-03-05 16:47:06 -0600

    finishing the clases

--- src/hrapp/HRApp.java
+++ src/hrapp/HRApp.java
@@ -16,13 +16,27 @@ public class HRApp {
     public static void main(String[] args) {
         System.out.println("HRApp Starts");
 
-        Employee e1 = new Employee(230, "Mario ", 15500);
-        Employee e2 = new Employee(340, "Laura ", 35000);
-        Employee e3 = new Employee(531, "Ale ", 800);
+        Employee e1 = new Employee(230, "Mario ", 15000);
+        Employee e2 = new Employee(340, "Laura ", 15000);
+        Employee e3 = new Employee(531, "Ale ", 15000);
 
-        System.out.println("Emp " + e1);
-        System.out.println("Emp " + e2);
-        System.out.println("Emp " + e3);
+        Department dept = new Department("Education");
+        dept.addEmp(e2);
+        dept.addEmp(e3);
+        dept.addEmp(new Employee(772, "Jack", 15000));
+
+        Employee[] emps = dept.getEmployees();
+
+        for (Employee emp : emps) {
+
+            System.out.println("Emp " + emp);
+
+        }
+        System.out.println("Department: " + dept.getName());
+        System.out.println("Total Salary of the employees are: " + dept.getTotalSalary());
+        System.out.println("The Avarage Salary is: " + dept.getAverageSalary());
+        
+        System.out.println("Emp " + dept.getEmployeeByID(531));
     }
 
 }
